015c427e81ddec20656f8a9ef46dc5cc5bb24408
xiaofang <fang3.xiao@tcl.com>
2019-01-14 09:55:14 +0800

    init commit
    Signed-off-by: xiaofang <fang3.xiao@tcl.com>

---
+++ widgetlibs/src/main/java/damon/ll/widgetlibs/progress/CircleProgressView.java
@@ -0,0 +1,118 @@
+package damon.ll.widgetlibs.progress;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+/**
+ * xiaofang
+ * 19-1-8
+ **/
+public class CircleProgressView extends FrameLayout {
+    private static final String TAG = "CircleProgressView";
+    private ImageView mProgressView;
+    private Context mContext;
+    private long mProgressPerTime;// 每转一圈的时间,毫秒数
+    private long mProgressTotalTime;// 一共转的时间,毫秒数
+    private int mCurrentProgress;
+    private int mProgressTotal;
+    private ProgressCallback mCallback;
+    private boolean mReverse;
+
+    public CircleProgressView(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public CircleProgressView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleProgressView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public CircleProgressView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mContext = context;
+        mProgressView = new ImageView(mContext);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        mProgressView.setLayoutParams(layoutParams);
+        addView(mProgressView);
+    }
+
+    public void setProgress(final long progressTotalTime, final int progressTotal) {
+        mProgressTotalTime = progressTotalTime;
+        if (progressTotal != 0) {
+            mProgressPerTime = mProgressTotalTime / progressTotal;
+        }
+        mProgressTotal = progressTotal;
+        mCurrentProgress = 0;
+    }
+
+    public void setProgressImageResource(int resourceId) {
+        mProgressView.setImageResource(resourceId);
+    }
+
+    public void setProgressBackgroundResource(int resourceId) {
+        setBackgroundResource(resourceId);
+    }
+
+    public void setReverse(boolean reverse) {
+        mReverse = reverse;
+    }
+
+    /**
+     * Must be called after {@link #setProgress(long, int)}
+     */
+    public void showProgress(final ProgressCallback callback) {
+        mCallback = callback;
+        final RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(mProgressPerTime);
+        rotate.setFillAfter(true);
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mCurrentProgress += 1;
+                int progress = mReverse ? (mProgressTotal - (mCurrentProgress -1)) : mCurrentProgress;
+                Log.d(TAG, "showProgress.onAnimationStart mCurrentProgress " + mCurrentProgress + ", mProgressTotal " + mProgressTotal + ",progress " + progress);
+                if (mCallback != null) {
+                    mCallback.onProgress(progress);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Log.d(TAG, "showProgress.onAnimationEnd mCurrentProgress " + mCurrentProgress + ", mProgressTotal " + mProgressTotal);
+                if (mCurrentProgress < mProgressTotal) {
+                    mProgressView.startAnimation(rotate);
+                }
+
+                if (mCallback != null && mCurrentProgress == mProgressTotal) {
+                    mCallback.onFinish();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        mProgressView.startAnimation(rotate);
+    }
+
+    public interface ProgressCallback {
+        void onProgress(int progress);
+
+        void onFinish();
+    }
+
+}
